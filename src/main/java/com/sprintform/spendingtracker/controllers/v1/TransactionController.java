36152c27ddd84232f5f605eb9e94902b487e10fc
jronyecz <ronyecz@gmail.com>
2023-10-13 22:00:51 +0200

    Add @CrossOrigin for local running with UI

--- src/main/java/com/sprintform/spendingtracker/controllers/v1/TransactionController.java
+++ src/main/java/com/sprintform/spendingtracker/controllers/v1/TransactionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(TransactionController.BASE_URL)
 public class TransactionController {
